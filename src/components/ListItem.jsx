a4b19a50b60c7d563eff2c77549a0ae3aa26bea1
TheShyen <ruslankaslesh@mail.ru>
2022-07-18 12:05:13 +0500

    Updates

--- src/components/ListItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListItem = (props) => {
-    return <li>{props.value}</li>;
-};
-
-export default ListItem;
--- src/components/SimpleFunc.js
+++
@@ -1,9 +0,0 @@
-function SimpleFunc(props) {
-    return (
-      <div>
-        <p>Function component. Test props value: {props.testValue}</p>
-      </div>
-    );
-  }
-  
-  export default SimpleFunc;
