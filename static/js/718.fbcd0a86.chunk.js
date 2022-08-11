1cd552da50e4effa37e062f6432267de100559cb
TheShyen <ruslankaslesh@mail.ru>
2022-08-11 10:49:56 +0500

    Updates

---
+++ static/js/718.fbcd0a86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[718],{718:function(e,n,u){u.r(n);var a=u(184);n.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h2",{children:"Lazy Item"})})}}}]);
+//# sourceMappingURL=718.fbcd0a86.chunk.js.map
